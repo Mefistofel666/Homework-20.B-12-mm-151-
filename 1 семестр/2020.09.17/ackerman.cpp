e719657f1d46d8dd9616a7f992d0183a752eadcd
Duvakin Kirill <duvakin.k@gmail.com>
2020-09-23 22:16:40 +0300

    hw

--- 1 семестр/2020.09.17/ackerman.cpp
+++ 1 семестр/2020.09.17/ackerman.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int ackerman_func(int m, int n){
-    if (m == 0) return n+1;
-    else if(m>0 && n == 0) return ackerman_func(m-1, 1);
-    else if (m>0 && n>0) return ackerman_func(m-1, ackerman_func(m,n-1));
+long int ackerman_func(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return ackerman_func(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return ackerman_func(m - 1, ackerman_func(m, n - 1));
     return 0;
 }
-int main(){
-    int m,n;
+int main()
+{
+    int m, n;
     cin >> m >> n;
-    int res = ackerman_func(n,m);
+    long int res = ackerman_func(n, m);
     cout << "res: " << res;
     return 0;
 }
