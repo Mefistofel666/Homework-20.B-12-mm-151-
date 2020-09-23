5a7dba8e5ed67bfdbe418e9412e25216ce32e2d7
Duvakin Kirill <duvakin.k@gmail.com>
2020-09-23 22:08:21 +0300

    homework

---
+++ 1 семестр/2020.09.17/average.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n;
+    float sum;
+    cout << "Write n: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        sum = sum + pow(i,3);
+    }
+    cout << "average: " << sum/n;
+    return 0;
+}
+
---
+++ 1 семестр/2020.09.17/binomial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float n, k, res = 1;
+  cout << "Write n, and after k \n" ;
+  cin >> n >> k;
+  for(int i = 0; i < k; i++){
+    res = res*(n-i)/(k-i);
+  }
+  cout << "result: " << res;
+  return 0;
+}
