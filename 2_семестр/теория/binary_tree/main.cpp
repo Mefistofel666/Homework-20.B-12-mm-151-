86272f9109221505b83678192f6af9675c523f49
Kirill <duvakin.k@gmail.com>
2021-05-22 18:51:06 +0300

    script

--- 2_семестр/теория/binary_tree/main.cpp
+++ 2_семестр/теория/binary_tree/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+//             4
+//         2       5
+//     1      3        7
+//                  6     8
+// 
+//     
     SearchTree *tree = new SearchTree(4);
     tree->insert(2);
     tree->insert(5);
@@ -10,8 +16,10 @@ int main()
     tree->insert(7);
     tree->insert(6);
     tree->insert(8);
+    std::cout << "In ascending order: \n";
     tree->inOrderTraversal("cout");
     std::cout << "\n";
+    std::cout << "By level: \n";
     tree->BFS();
     std::cout << "\n";
 
